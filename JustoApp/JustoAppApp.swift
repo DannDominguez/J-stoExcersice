5958d5e656ec9f53766c120d0815aca7bd092a17
Daniela Ciciliano <dann@Danielas-MacBook-Air.local>
2024-04-17 18:44:49 -0600

    First Commit

---
+++ JustoApp/JustoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustoAppApp.swift
+//  JustoApp
+//
+//  Created by Daniela Ciciliano on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JustoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
